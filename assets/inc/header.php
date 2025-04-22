ff7faf0bb55ba6062e3dd808fe327b2a9855fe1b
Lukas Mandok <mandok@tphysi.uni-heidelberg.de>
2025-04-22 17:58:10 +0200

    transition navbar to bootstrap

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -4,53 +4,65 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Website Header</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo $headerCss;?>">    
 </head>
 
 <body>
 
 <header>
-    <nav class="tab-bar" style="text-align: center;">
-        <div class="logo">
-            <a href="https://www.physi.uni-heidelberg.de/"> <!-- Add link here -->
-                <img src="<?php echo $figures;?>/logo-pi.png" alt="Logo">
+    <!-- Bootstrap Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light"> <!-- Adjust classes (e.g., navbar-dark bg-dark) as needed -->
+        <div class="container-fluid"> <!-- Use container-fluid for full width -->
+            <a class="navbar-brand" href="https://www.physi.uni-heidelberg.de/">
+                <img src="<?php echo $figures;?>/logo-pi.png" alt="Logo" height="30"> <!-- Adjust height as needed -->
             </a>
+
+            <!-- Bootstrap Hamburger Button -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- Navbar Links -->
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0"> <!-- Added ms-auto back -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo dirname($subpath);?>/main_page.php">Home</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="researchDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" tabindex="-1" aria-disabled="true"> <!-- Added tabindex and aria-disabled -->
+                            Research
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="researchDropdown">
+                            <li><a class="dropdown-item" href="<?php echo dirname($subpath);?>/research/mu3e/mu3e_mainpage.php">Mu3e</a></li>
+                            <li><a class="dropdown-item" href="<?php echo dirname($subpath);?>/research/ATLAS/ATLAS_mainpage.php">ATLAS</a></li>
+                            <li><a class="dropdown-item" href="<?php echo dirname($subpath);?>/research/HV-Maps/HV-Maps_mainpage.php">HV-MAPS</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="publicationsDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" tabindex="-1" aria-disabled="true"> <!-- Added tabindex and aria-disabled -->
+                            Publications
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="publicationsDropdown">
+                            <li><a class="dropdown-item" href="<?php echo dirname($subpath);?>/publications/mu3e_pub.php">Mu3e</a></li>
+                            <li><a class="dropdown-item" href="<?php echo dirname($subpath);?>/publications/atlas_pub.php">ATLAS</a></li>
+                            <li><a class="dropdown-item" href="<?php echo dirname($subpath);?>/publications/hvmaps_pub.php">HV-MAPS</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo dirname($subpath);?>/teaching.php">Teaching</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo dirname($subpath);?>/people.php">People</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo dirname($subpath);?>/positions.php">Theses Opportunities</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-
-        <!-- Hamburger Menu Button -->
-        <button class="hamburger" onclick="toggleMenu()">☰</button>
-
-        <ul class="menu">
-            <li><a href="<?php echo dirname($subpath);?>/main_page.php">Home</a></li>
-            <li class="dropdown">
-                <!-- this style prevents from clicking on the word in the menu -->
-                <a style="pointer-events: none; cursor: default;" href="#">Research</a>
-                <ul class="dropdown-menu">
-                    <li><a href="<?php echo dirname($subpath);?>/research/mu3e/mu3e_mainpage.php">Mu3e</a></li>
-                    <li><a href="<?php echo dirname($subpath);?>/research/ATLAS/ATLAS_mainpage.php">ATLAS</a></li>
-                    <li><a href="<?php echo dirname($subpath);?>/research/HV-Maps/HV-Maps_mainpage.php">HV-MAPS</a></li>
-                </ul>
-            </li>
-            <li class="dropdown"> 
-                <!-- this style prevents from clicking on the word in the menu -->
-                <a style="pointer-events: none; cursor: default;" href="#">Publications</a>
-                <ul class="dropdown-menu">
-                    <li><a href="<?php echo dirname($subpath);?>/publications/mu3e_pub.php">Mu3e</a></li>
-                    <li><a href="<?php echo dirname($subpath);?>/publications/atlas_pub.php">ATLAS</a></li>
-                    <li><a href="<?php echo dirname($subpath);?>/publications/hvmaps_pub.php">HV-MAPS</a></li>
-                </ul>
-            </li>
-            <li><a href="<?php echo dirname($subpath);?>/teaching.php">Teaching</a></li>
-            <li><a href="<?php echo dirname($subpath);?>/people.php">People</a></li>
-            <li><a href="<?php echo dirname($subpath);?>/positions.php">Theses Opportunities</a></li>
-        </ul>
     </nav>
 </header>
-
-
-<!-- JavaScripts -->
-<script type="text/javascript" src="<?php echo $menu_mobile_toggleJs;?>"></script>
-
 
 </body>
 </html>
